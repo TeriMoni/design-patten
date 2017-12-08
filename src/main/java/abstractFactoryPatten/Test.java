8ba78746e54bb5415ad933e2cb8d10866038c4c6
TeriMoni <351111617@qq.com>
2017-12-08 16:57:28 +0800

    测试类

--- src/main/java/abstractFactoryPatten/Test.java
+++ src/main/java/abstractFactoryPatten/Test.java
@@ -9,7 +9,7 @@ import decoratorPatten.Beverage;
  **/
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
         BeverageFactory beverageFactory = new HotDrinkFactory();
         Coffee hotCoffee = beverageFactory.createCoffeeDrink("拿铁咖啡");
         MilkTea hotMilkTea = beverageFactory.createMilkTeaDrink("慕斯奶茶");
@@ -17,5 +17,14 @@ public class Test {
         BeverageFactory iceDrinkFactory = new IceDrinkFactory();
         Coffee iceCoffee = iceDrinkFactory.createCoffeeDrink("卡布奇洛");
         MilkTea iceMilkTea = iceDrinkFactory.createMilkTeaDrink("红豆布丁奶茶");
+        System.out.println("反射创建工厂类");
+        SampleFactory sampleFactory = new SampleFactory();
+        sampleFactory.setDrinkType("HotDrink");
+        HotDrinkFactory hotDrinkFactory = sampleFactory.createHotDrinkFactory();
+        hotDrinkFactory.createMilkTeaDrink("芒果奶茶");
+        System.out.println("===========================");
+        sampleFactory.setDrinkType("IceDrink");
+        IceDrinkFactory iceDrinkFactory2 = sampleFactory.createIceDrinkFactory();
+        iceDrinkFactory2.createCoffeeDrink("香浓咖啡");
     }
 }
