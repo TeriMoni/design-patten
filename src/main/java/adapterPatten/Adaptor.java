c6b71e1d652b6bd30ed65852a9292948d15f58b6
TeriMoni <351111617@qq.com>
2017-12-14 17:03:14 +0800

    适配类

---
+++ src/main/java/adapterPatten/Adaptor.java
@@ -0,0 +1,13 @@
+package adapterPatten;
+
+/**
+ * @Description: 适配器，usb转接线
+ * @author: liubin
+ * @create: 2017-12-14 16:26
+ **/
+public class Adaptor {
+
+    public void convert(){
+        System.out.print("通过usb转接线转换接口：");
+    }
+}
