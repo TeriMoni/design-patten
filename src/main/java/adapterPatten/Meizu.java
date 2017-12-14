53c07c2c28b630ff38e8943a8890fe668808eeff
TeriMoni <351111617@qq.com>
2017-12-14 17:03:36 +0800

    魅族手机

---
+++ src/main/java/adapterPatten/Meizu.java
@@ -0,0 +1,20 @@
+package adapterPatten;
+
+/**
+ * @Description: 魅族手机
+ * @author: liubin
+ * @create: 2017-12-14 16:25
+ **/
+public class Meizu extends Phone implements Usb {
+
+    private Adaptor adaptor;
+
+    public Meizu(String brand,Adaptor adaptor) {
+        this.brand = brand;
+        this.adaptor = adaptor;
+    }
+    public void charge() {
+        adaptor.convert();
+        System.out.println(brand+"通过手机usb充电");
+    }
+}
