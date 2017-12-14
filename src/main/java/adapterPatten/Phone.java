fcfb6a673f73e610d797476d1bda200f4c692a0b
TeriMoni <351111617@qq.com>
2017-12-14 17:03:47 +0800

    手机父类

---
+++ src/main/java/adapterPatten/Phone.java
@@ -0,0 +1,29 @@
+package adapterPatten;
+
+/**
+ * @Description: 手机父类
+ * @author: liubin
+ * @create: 2017-12-14 16:52
+ **/
+public class Phone {
+
+    public String brand;
+
+    public String price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
